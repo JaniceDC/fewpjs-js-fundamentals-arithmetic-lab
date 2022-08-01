6e7286eb22ca5f9ca094c08242c26e63df976de4
JaniceDC <janice.dipayan.chen@gmail.com>
2022-08-01 21:13:10 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const chai = chai
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2;
+
+const random = Math.floor (Math.random () * 10) +1;
+
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(2, 6, 8, 11, 17, 20)
